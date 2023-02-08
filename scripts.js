ec8d6c5c9a0fb874f31593d61cd3de4623ae5a03
Patrícia Silva <ps397380@gmail.com>
2023-02-08 20:33:12 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,33 +1,37 @@
 // VARIÁVEIS => Um espaço da memória do computador que guardamos algo (um numero, uma letra, um texto, uma imagem)
 // FUNÇÃO => Um trecho de código que só é executado quando é chamado
 
-let chave = "cebcd482eda57fa9a6714c1c2ba91885"
 
 
-function colocarNaTela(dados){
+let key = "f8e92003f5a3ab04f18d02d1928c6b06"
+
+const putScreen= (dado) =>{
+
+    document.querySelector(".city").innerHTML = "Tempo em " + dado.name
+    document.querySelector(".temp").innerHTML = Math.floor( dado.main.temp) + "°C"
+    document.querySelector(".clouds").innerHTML = dado.weather[0].description
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dado.weather[0].icon + '.png'
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dado.main.humidity + '%'
+
+}
+
+async function  searchCity(cityh)  {
+
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityh 
+     + "&appid=" + key + "&lang=pt_br" + "&units=metric")
+    .then( result =>  result.json())
+
+    putScreen(dados)
+   
     console.log(dados)
-    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-    document.querySelector(".temp").innerHTML =  Math.floor(dados.main.temp) + "°C"
-    document.querySelector(".descricao").innerHTML = dados.weather[0].description
-    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
-}
-
-async function buscarCidade(cidade){
-    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
-    cidade + 
-    "&appid=" + 
-    chave + 
-    "&lang=pt_br" +
-    "&units=metric"
-    )
-    .then(resposta => resposta.json())
-
-    colocarNaTela(dados)
 }
 
 
-function cliqueiNoBotao(){
-   let cidade = document.querySelector(".input-cidade").value
+const clickedBhutan = ()=>{
 
-   buscarCidade(cidade)
+    let city = document.querySelector(".search").value 
+
+    searchCity(city)
+    
+    
 }
